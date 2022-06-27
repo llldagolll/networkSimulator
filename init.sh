ca6e31b15cb83cc61f0d48cbc81c6f8900cb3d8b
llldagolll <anobu4@gmail.com>
2022-06-27 11:32:41 +0900

    add backend

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #/bin/bash
+set -eu
 
+echo 'frontend'
 docker-compose run -w /app --rm frontend-senior-project npm install
+echo 'backend'
+docker-compose run -w /app --rm backend-senior-project npm install
