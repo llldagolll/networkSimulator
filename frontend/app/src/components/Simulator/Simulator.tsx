f8ebfc03de90c1660907782e69f59b3a2e5c5d92
nobuya <72905484+llldagolll@users.noreply.github.com>
2022-07-09 15:40:08 +0900

    modify defining node style
    css -> styled component

--- frontend/app/src/components/Simulator/Simulator.tsx
+++ frontend/app/src/components/Simulator/Simulator.tsx
@@ -1,21 +1,31 @@
-import { useRef, useState, useCallback, useEffect } from "react";
-import ReactFlow, { ReactFlowProvider, Controls } from "react-flow-renderer";
+import { useRef, useState, useCallback } from 'react';
+import ReactFlow, { ReactFlowProvider, Controls } from 'react-flow-renderer';
+import useGenerateFormValue from 'lib/generateFormValue';
 import styles from './Simulator.module.css'
-import useGenerateFormValue from "../../../lib/generateFormValue";
-import { NewNode } from "./types";
-import { CustomNodeTypes } from "./CustomNodeTypes";
-import useStore from "../../store";
+import { NewNode } from './types';
+import { CustomNodeTypes } from './CustomNodeTypes';
+import useStore from '@/store';
 
 
+interface Style {
+  padding: number;
+  borderRadius: number;
+  borderWidth: number;
+  borderStyle: string;
+  borderColor: string;
+  fontSize: string;
+  color?: string;
+  textAlign?: string
+  width?: string;
+  height?: string;
+}
 
 let id = 0;
 const generateId = () => `${id++}`;
 
-
 const Simulator = () => {
-
   const reactFlowWrapper = useRef(null);
-  const [reactFlowInstance, setReactFlowInstance] = useState(null)
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
   const {
     nodes,
     edges,
@@ -24,12 +34,14 @@ const Simulator = () => {
     onConnect,
     setNodes,
     setLans,
-  } = useStore()
+  } = useStore();
 
-  const onDragOver = useCallback((event: { preventDefault: () => void; dataTransfer: { dropEffect: string; }; }) => {
-    event.preventDefault()
+  const onDragOver = useCallback((event: {
+    preventDefault: () => void; dataTransfer: { dropEffect: string; };
+  }) => {
+    event.preventDefault();
     event.dataTransfer.dropEffect = 'move'
-  }, [])
+  }, []);
 
   const onDrop: any = useCallback(
     (
@@ -37,9 +49,9 @@ const Simulator = () => {
         preventDefault: () => void; dataTransfer: { getData: (arg0: string) => any; }; clientX: number; clientY: number;
       },
     ) => {
-      event.preventDefault()
-      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect()
-      const type = event.dataTransfer.getData('application/reactflow')
+      event.preventDefault();
+      const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+      const type = event.dataTransfer.getData('application/reactflow');
 
       if (typeof type === 'undefined' || !type) {
         return;
@@ -48,9 +60,36 @@ const Simulator = () => {
       const position = reactFlowInstance.project({
         x: event.clientX - reactFlowBounds.left,
         y: event.clientY - reactFlowBounds.top,
-      })
+      });
 
       const id = generateId()
+
+      const baseStyle: Style = {
+        padding: 10,
+        borderRadius: 3,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#1a192b',
+        fontSize: '12px',
+        color: '#222',
+      }
+
+      const nodeStyle = {
+        ...baseStyle,
+        width: '60px',
+        background: '#fff',
+        textAlign: 'center',
+      }
+
+      const lanStyle = {
+        ...baseStyle,
+        width: '150px',
+        height: '200px',
+        background: 'rgba(255,0,0,0.2)'
+      }
+
+      let style
+      type === 'Lan' ? style = lanStyle : style = nodeStyle
 
       const newNode: NewNode = {
         id,
@@ -61,6 +100,7 @@ const Simulator = () => {
           id,
         },
         position,
+        style,
       }
 
       const defaultFormValue = useGenerateFormValue({ id, type })
